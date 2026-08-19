1055c3084b5df4040f96f195c920ee2bba6ebc7f
nameeaaa <alrletsheadthere@gmail.com>
2026-08-20 00:11:57 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const mineflayer = require('mineflayer');
+
+const botOptions = {
+  host: 'minigames.mcsh.io', // <-- Put your Paper server IP here
+  port: 12930,                  // <-- Change if your server uses a custom port
+  username: 'ikeepthebebion',    // <-- The name your bot will use on the server
+  auth: 'offline'               // Tells the bot it is a cracked server
+};
+
+function createBot() {
+  const bot = mineflayer.createBot(botOptions);
+
+  bot.on('spawn', () => {
+    console.log('Bot has joined the server!');
+
+    // Jump and look around every 15 seconds to stay active
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      setTimeout(() => bot.setControlState('jump', false), 500);
+      bot.look(Math.random() * 360, 0);
+    }, 15000);
+  });
+
+  // Automatically reconnect if the server restarts or kicks the bot
+  bot.on('end', () => {
+    console.log('Disconnected. Reconnecting in 10 seconds...');
+    setTimeout(createBot, 10000);
+  });
+
+  bot.on('error', (err) => console.log('Error:', err));
+}
+
+createBot();
