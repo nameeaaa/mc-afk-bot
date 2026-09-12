4eca761db7f369c2621583d76a906290b24f007c
nameeaaa <alrletsheadthere@gmail.com>
2026-09-12 21:20:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.listen(process.env.PORT || 4000, () => {
 
 // 2. Your Minecraft Bot configuration
 const botOptions = {
-  host: '8pythagorasmp6.mcsh.io', 
+  host: '8pythagorassmp6.mcsh.io', 
   username: 'mrwonderland', 
   auth: 'offline',
   version: '1.21.11'
